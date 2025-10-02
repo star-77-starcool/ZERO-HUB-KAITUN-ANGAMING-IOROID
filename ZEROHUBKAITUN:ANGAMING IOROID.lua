48c71d7c5bbd13b99a02f183ce77f2b191bd46a4
ANGAMING IOROID <angamingioroid@icloud.com>
2025-10-02 11:33:00 +0700

    Update ZEROHUBKAITUN:ANGAMING IOROID.lua

--- ZEROHUBKAITUN:ANGAMING IOROID.lua
+++ ZEROHUBKAITUN:ANGAMING IOROID.lua
@@ -1,1 +1,3 @@
-{"c즊mmުy؟]zWzbXzZvkx jgFzm?b櫚zߊW?z?qzq6^^ӿ_k׍ٶ
+script_key = "KEY"; -- A key might be required, if not, delete this line.
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/ca16cb42816b395e079fa7a114352ba9.lua"))()
+ 
